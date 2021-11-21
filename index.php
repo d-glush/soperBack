664aaa7c92c4a2bad309b8b6fda6bee00ed4e2f1
d-glush <90252922+d-glush@users.noreply.github.com>
2021-11-21 15:05:12 +0300

    refactored

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ use game\GameRoute;
 
 require_once 'core/init.php';
 
+header('Access-Control-Allow-Origin: *');
 header('Content-type: application/json; charset=UTF-8');
 session_start();
 
@@ -16,16 +17,16 @@ switch($method) {
     case 'game':
         $gameRoute = new GameRoute($extQueryPath);
         $result = $gameRoute->process();
-        $response = json_encode($result);
-        echo $response;
         break;
     case 'account':
-        $accountRoute = new AccountRoute($extQueryPath);
-        $result = $accountRoute->process();
-        $response = json_encode($result);
-        echo $response;
+        $saveResultRoute = new SaveResultRoute($extQueryPath);
+        $result = $saveResultRoute->process();
         break;
     default:
         http_response_code(404);
         echo json_encode('wrong method name');
+        exit;
 }
+
+$response = json_encode($result);
+echo $response;
